0c141de85a74a0bfd011b96dec0707936a474786
Vadym <vadikdeputat@gmail.com>
2020-10-15 15:17:16 +0200

    Add convert to PLN

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.widget.Toast.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void convert(View view) {
         EditText amountEditText = (EditText) findViewById(R.id.amountEditText);
-        Log.i("ammount", amountEditText.getText().toString());
+
+        Double amountDouble = Double.parseDouble(amountEditText.getText().toString());
+
+        Double polishAmount = amountDouble * 3.5;
+
+        Toast.makeText(MainActivity.this, polishAmount.toString() + " in Dollars: " + polishAmount.toString(), LENGTH_SHORT).show();
 ;    }
 }
